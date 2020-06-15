d3241ce11a88d26f4289c4cb9e50b48dc02a6d34
Kainette <moceri.marine@gmail.com>
2020-06-15 15:59:57 +0200

    menu-burger-js

--- content/themes/belleenboucles/app/js/app.js
+++ content/themes/belleenboucles/app/js/app.js
@@ -1,7 +1,32 @@
 var app = {
-  init: function() {
+  init: function () {
+    app.initEventListener();
     console.log('init');
-  }
-};
+  },
 
-$(app.init);
+  initEventListener: function () {
+
+    let burgerMenuOpenAndCloseButton = document.querySelector('#i');
+    burgerMenuOpenAndCloseButton.addEventListener('click', app.handleOpenAndCloseMenu);
+  },
+
+  handleOpenAndCloseMenu: function () {
+    console.log('click');
+    // let sidebarMenu = document.querySelector('.sidebar__nav');
+    let sidebar = document.querySelector('.sidebar__nav');
+    sidebar.classList.toggle('open');
+    let icon = document.getElementById('i');
+
+    if (icon.classList.contains('fa-times') && sidebar.classList.contains('open')) {
+      icon.className = 'fa fa-bars';
+    }
+    else if (icon.classList.contains('fa-bars') && sidebar.classList != 'open') {
+      icon.className = 'fa fa-times';
+    } else {
+      icon.className = 'fa fa-bars';
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", app.init);
+
