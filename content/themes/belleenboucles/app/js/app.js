1391744ad84934c2c32f99ea00dd396b5f355a7d
Kainette <moceri.marine@gmail.com>
2020-06-16 16:55:39 +0200

    header-scroll

--- content/themes/belleenboucles/app/js/app.js
+++ content/themes/belleenboucles/app/js/app.js
@@ -1,6 +1,7 @@
 var app = {
   init: function () {
     app.initEventListener();
+    app.HandleStickyHeader();
     console.log('init');
   },
 
@@ -8,6 +9,8 @@ var app = {
 
     let burgerMenuOpenAndCloseButton = document.querySelector('#i');
     burgerMenuOpenAndCloseButton.addEventListener('click', app.handleOpenAndCloseMenu);
+    //window.addEventListener('scroll' , app.HandleStickyHeader);
+    
   },
 
   handleOpenAndCloseMenu: function () {
@@ -25,8 +28,51 @@ var app = {
     } else {
       icon.className = 'fa fa-bars';
     }
-  }
-}
+  },
 
+  HandleStickyHeader: function () {
+    
+
+    // let header = document.querySelector('.header');
+    // let lastScrollValue = 0;
+    // let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    // console.log(scrollTop)
+    
+    // if (scrollTop > lastScrollValue) {
+    //   header.classList.add('hidden');
+    // }else if (window.scrollTop) {
+    //   header.classList.remove('hidden');
+    // } else {
+    //   header.classList.remove('hidden');
+    // }
+    // lastScrollValue = scrollTop;
+
+      const header = document.querySelector('header');
+      const scrollUp = "scroll-up";
+      const scrollDown = "scroll-down";
+      let lastScroll = 0;
+      
+      window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
+        if (currentScroll == 0) {
+          header.classList.remove(scrollUp);
+          return;
+        }
+        
+        if (currentScroll > lastScroll && !header.classList.contains(scrollDown)) {
+          // down
+          header.classList.remove(scrollUp);
+          header.classList.add(scrollDown);
+        } else if (currentScroll < lastScroll && header.classList.contains(scrollDown)) {
+          // up
+          header.classList.remove(scrollDown);
+          header.classList.add(scrollUp);
+        }
+        lastScroll = currentScroll;
+      });
+  }
+
+}
+ 
 document.addEventListener("DOMContentLoaded", app.init);
 
